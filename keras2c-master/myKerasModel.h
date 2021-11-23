c3ff07587d4e879e85a2d5235546568def78ebd6
nguyenhuynhthien <nguyenhuynhthien91@gmail.com>
2021-11-23 16:38:41 +0700

    prepare for uploading

--- keras2c-master/myKerasModel.h
+++
@@ -1,5 +0,0 @@
-#pragma once 
-#include "./include/k2c_tensor_include.h" 
-void myKerasModel(k2c_tensor* dense_6_input_input, k2c_tensor* dense_8_output); 
-void myKerasModel_initialize(); 
-void myKerasModel_terminate(); 
